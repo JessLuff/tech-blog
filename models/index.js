9fc7f5520700be34d34d767f2a7bbcc8104c2cbd
jessluff <jessica.luff@outlook.com>
2021-08-02 02:20:21 +1000

    Added comment connections to models/index.js

--- models/index.js
+++ models/index.js
@@ -7,6 +7,16 @@ User.hasMany(Post, {
   onDelete: 'CASCADE'
 });
 
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
+});
+
 Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
